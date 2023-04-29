207c9ec425ff4e8a14a04b9e4bfe5c32313cd75f
Jesse Aguilar <jaguilar12@comcast.net>
2023-04-28 22:24:20 -0600

    Navigation Part 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,26 +1,36 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
 
-const DirectoryScreen = (props) => {
-  const renderDirectoryItem = ({ item: campsite }) => {
+const DirectoryScreen = ({ navigation }) => {
+    const [campsites, setCampites] = useState(CAMPSITES);
+
+    const renderDirectoryItem = ({ item: campsite }) => {
+        return (
+            <ListItem
+                onPress={() =>
+                    navigation.navigate("CampsiteInfo", { campsite })
+                }
+            >
+                <Avatar source={campsite.image} rounded />
+                <ListItem.Content>
+                    <ListItem.Title>{campsite.name}</ListItem.Title>
+                    <ListItem.Subtitle>
+                        {campsite.description}
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>
+        );
+    };
+
     return (
-      <ListItem onPress={() => props.onPress(campsite.id)}>
-        <Avatar source={campsite.image} rounded />
-        <ListItem.Content>
-          <ListItem.Title>{campsite.name}</ListItem.Title>
-          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-        </ListItem.Content>
-      </ListItem>
+        <FlatList
+            data={campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor={(item) => item.id.toString()}
+        />
     );
-  };
-
-  return (
-    <FlatList
-      data={props.campsites}
-      renderItem={renderDirectoryItem}
-      keyExtractor={(item) => item.id.toString()}
-    />
-  );
 };
 
 export default DirectoryScreen;
