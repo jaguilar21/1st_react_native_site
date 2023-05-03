eedf65c85d5d1637ee095ca11474bf7d657e7a48
Jesse Aguilar <jaguilar12@comcast.net>
2023-05-03 17:37:22 -0600

    Using Redux, fixed errors

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { campsiteReducer } from '../features/campsites/campsitesSlice';
+import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
 export const store = configureStore({
 	reducer: {
-		campsites: campsiteReducer,
+		campsites: campsitesReducer,
 		comments: commentsReducer,
 		partners: partnersReducer,
 		promotions: promotionsReducer
